71e131c846878c4c9ebf90974efffa84afbf2811
nalpari <yoo32767@gmail.com>
2026-03-15 06:37:00 +0900

    fix: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { hostname: "lh3.googleusercontent.com" },
+      { hostname: "avatars.githubusercontent.com" },
+    ],
+  },
 };
 
 export default nextConfig;
